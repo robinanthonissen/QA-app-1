77e000d48804a802516400dd39225849453eaae6
Robin Anthonissen <robinanthonissen@hotmail.com>
2017-11-21 14:39:28 +0100

    werkende database

---
+++ QA-app-1/Config.cs
@@ -0,0 +1,70 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QA_app_1
+{
+    public class Config
+    {
+        //clients en customize
+
+
+        Secret secret = new Secret("secret".Sha256(), "2017 secret", new DateTime(2017, 11, 1));
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResource("roles", new List<string> { "role" })
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("WebAPI" ) {
+                    UserClaims = { "role" }
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            // Clients credentials.
+            return new List<Client>
+            {
+                // http://docs.identityserver.io/en/release/reference/client.html.
+                new Client
+                {
+                    ClientId = "TestBackend",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword, // Resource Owner Password Credential grant.
+                    AllowAccessTokensViaBrowser = true,
+                    RequireClientSecret = false, // This client does not need a secret to request tokens from the token endpoint.
+
+                    AccessTokenLifetime = 3600, // Lifetime of access token in seconds.
+
+                    AllowedScopes = {
+                        IdentityServerConstants.StandardScopes.OpenId, // For UserInfo endpoint.
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "roles",
+                        "WebAPI"
+                    },
+                    AllowOfflineAccess = true, // For refresh token.
+                    RefreshTokenUsage = TokenUsage.OneTimeOnly,
+                    AbsoluteRefreshTokenLifetime = 7200,
+                    SlidingRefreshTokenLifetime = 900,
+                    RefreshTokenExpiration = TokenExpiration.Sliding,
+                }
+
+
+            };
+        }
+
+    }
+}
---
+++ QA-app-1/Controllers/UserController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using QA_app_1.Models;
+using QA_app_1.Models.PostModels;
+
+namespace QA_app_1.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/User")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+
+        }
+
+        [HttpGet("GetUser")]
+        public async Task<IActionResult> getuser()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            return new JsonResult(user);
+        }
+
+        //[AllowAnonymous]
+        [HttpPost("CreateUser")]
+        public async Task<IActionResult> createuser([FromBody] CreateUserModel model)
+        {
+            try
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.username,
+                    voornaam = model.firstName,
+                    achternaam = model.lastName,
+                    Email = model.email
+                };
+
+                if (model.password1 != model.password2)
+                {
+                    return new BadRequestObjectResult("passwoorden zijn niet gelijk");
+                }
+
+                var result = await _userManager.CreateAsync(user, model.password1);
+
+                if (result.Succeeded)
+                {
+                    var toegevoegdeUser = await _userManager.FindByIdAsync(user.Id);
+                    var assignUserToRole = await _userManager.AddToRoleAsync(toegevoegdeUser, "user");
+
+                    if (assignUserToRole.Succeeded)
+                    {
+                        return new OkObjectResult("User toegevoegd aan role");
+                    }
+
+                }
+
+                return new BadRequestObjectResult("failed");
+            }
+            catch (Exception e)
+            {
+                return new BadRequestObjectResult(e.ToString());
+            }
+        }
+    }
+}
